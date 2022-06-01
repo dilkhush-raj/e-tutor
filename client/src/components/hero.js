30ff6208423c65bed0eab9a210b6586172ecde33
Dilkhush Raj <dilkhush_raj@outlook.com>
2022-06-01 20:51:56 +0530

    fixing error

--- client/src/components/hero.js
+++ client/src/components/hero.js
@@ -1,11 +1,15 @@
+import React from "react";
+
 export default function Hero() {
-    return (
-      <div className="hero">
-        <div className="content">
-          <h1>Welcome to e-Tutor</h1>
-          <p>We have the best tutor to teach you.</p>
-        </div>
-        <div className="image"><img src="/images/hero.svg" alt="Hero-image" /></div>
+  return (
+    <div className="hero">
+      <div className="content">
+        <h1>Welcome to e-Tutor</h1>
+        <p>We have the best tutor to teach you.</p>
       </div>
-    );
-  }
+      <div className="image">
+        <img src="/images/hero.svg" alt="Hero-image" />
+      </div>
+    </div>
+  );
+}
